0c3bd0e2e541e5a1319fc841b7861b609e14dfff
Hoàng Văn Kiên <kiendaotac92@gmail.com>
2022-03-04 00:24:14 +0700

    add ip to report and change mobile blade

--- app/Http/Livewire/Admin/Account/Index.php
+++ app/Http/Livewire/Admin/Account/Index.php
@@ -10,24 +10,27 @@ class Index extends Component
     public function render()
     {
         $accounts = Account::query()->latest()->paginate(10);
+
         return view('livewire.admin.account.index', compact('accounts'))->layout('components.layouts.app');
     }
 
     public function download()
     {
         $accounts = Account::where('status', 'active')->get();
-        $content = "";
+        $content  = "";
 
-        foreach ($accounts as $account){
-            $username       = $account->username;
-            $password       = $account->password;
-            $time           = $account->created_at->format('d/m/Y - H:i:s');
-            $address        = $account->address;
-            $content .= "$username|$password|$time|$address\n";
+        foreach ($accounts as $account) {
+            $username = $account->username;
+            $password = $account->password;
+            $time     = $account->created_at->format('d/m/Y - H:i:s');
+            $address  = $account->address;
+            $ip       = $account->ip;
+            $content  .= "$username|$password|$ip|$time|$address\n";
         }
 
-        $fileName = "account-".date("Y-m-d-H-i-s").".txt";
+        $fileName = "account-" . date("Y-m-d-H-i-s") . ".txt";
         Account::query()->update(['status' => 'inactive']);
+
         return response()->streamDownload(function () use ($content) {
             echo $content;
         }, $fileName);
